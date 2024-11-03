22ebe98b068ab796d17d94ed434032faaf90204d
Aleksandr787 <shuverov.aleksandr@gmail.com>
2024-11-03 13:57:52 +0300

    master: add swager first step

--- src/app/presentation/layouts/provider-adding/provider-adding.component.ts
+++ src/app/presentation/layouts/provider-adding/provider-adding.component.ts
@@ -4,20 +4,32 @@ import { RouterModule } from '@angular/router';
 import { ProviderService } from '@domain/services/provider.service';
 import { IconType } from '@presentation/utils/icon/icon-type.enum';
 import { IconComponent } from '@presentation/utils/icon/icon.component';
-
+declare const SwaggerEditorBundle: any;
+declare const SwaggerEditorStandalonePreset: any;
 @Component({
   selector: 'app-provider-adding',
   standalone: true,
   imports: [CommonModule, RouterModule, IconComponent],
   template: `
     <div class="container-main">
-      <h1>Add your new provide!</h1>
+      <div id="swagger-editor"></div>
     </div>
-  `,
+    `,
   styleUrl: 'provider-adding.component.scss',
 })
 export class ProviderAddingComponent {
   private readonly _service = inject(ProviderService);
 
   protected readonly IconType = IconType;
+
+  ngOnInit(): void {
+    const editor = SwaggerEditorBundle({
+      dom_id: '#swagger-editor',
+      layout: 'StandaloneLayout',
+      presets: [
+        SwaggerEditorStandalonePreset
+      ],
+      url: 'http://rackerlabs.github.io/wadl2swagger/openstack/swagger/dbaas.json'
+    });
+  }
 }
