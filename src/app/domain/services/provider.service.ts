dae56ea3f7c1738d68409342b6de17039357423e
andrey_fesunov <fsnv04@gmail.com>
2024-11-03 18:46:25 +0300

    add swagger submit method

--- src/app/domain/services/provider.service.ts
+++ src/app/domain/services/provider.service.ts
@@ -1,13 +1,15 @@
 import { Injectable, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { ProviderCreateRequest } from '@domain/dtos/requests/provider-create.request';
 import { ProviderCreateResponse } from '@domain/dtos/responses/provider-create.response';
 import { ProviderLight } from '@domain/models/provider';
 import { ProviderRepository } from '@domain/repositories/provider.repository';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ProviderService {
   private readonly _repository = inject(ProviderRepository);
+  private readonly _router = inject(Router);
 
   public get(): Observable<readonly ProviderLight[]> {
     return this._repository.get();
@@ -16,6 +18,8 @@ export class ProviderService {
   public create(
     request: ProviderCreateRequest
   ): Observable<ProviderCreateResponse> {
-    return this._repository.create(request);
+    return this._repository
+      .create(request)
+      .pipe(tap(() => this._router.navigate(['/providers'])));
   }
 }
--- src/app/presentation/layouts/provider-adding/provider-adding.component.ts
+++ src/app/presentation/layouts/provider-adding/provider-adding.component.ts
@@ -1,8 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewEncapsulation, inject } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  ViewEncapsulation,
+  inject,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ProviderCreateRequest } from '@domain/dtos/requests/provider-create.request';
 import { ProviderService } from '@domain/services/provider.service';
-import { IconType } from '@presentation/utils/icon/icon-type.enum';
+import { getStorageState } from '@infrastructure/states/storage.state';
 import { IconComponent } from '@presentation/utils/icon/icon.component';
 declare const SwaggerEditorBundle: any;
 declare const SwaggerEditorStandalonePreset: any;
@@ -21,15 +29,60 @@ declare const SwaggerEditorStandalonePreset: any;
 })
 export class ProviderAddingComponent {
   private readonly _service = inject(ProviderService);
+  private readonly _destroyRef = inject(DestroyRef);
 
-  protected readonly IconType = IconType;
+  private readonly _key = 'swagger-editor-content';
 
-  ngOnInit(): void {
+  protected readonly form = new FormGroup({
+    name: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    clientId: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    clientSecret: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    authorizationEndpoint: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    tokenEndpoint: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    scope: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+  });
+
+  public ngOnInit(): void {
     SwaggerEditorBundle({
       dom_id: '#swagger-editor',
       layout: 'StandaloneLayout',
       presets: [SwaggerEditorStandalonePreset],
       url: 'http://rackerlabs.github.io/wadl2swagger/openstack/swagger/dbaas.json',
     });
+  }
+
+  protected submit(): void {
+    const request: ProviderCreateRequest = {
+      name: this.form.controls.name.value,
+      clientId: this.form.controls.clientId.value,
+      clientSecret: this.form.controls.clientSecret.value,
+      authorizationEndpoint: this.form.controls.authorizationEndpoint.value,
+      tokenEndpoint: this.form.controls.tokenEndpoint.value,
+      scope: this.form.controls.scope.value,
+      swaggerJson: getStorageState().getItem(this._key) ?? '{}',
+    };
+
+    this._service
+      .create(request)
+      .pipe(takeUntilDestroyed(this._destroyRef))
+      .subscribe();
   }
 }
